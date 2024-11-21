08322251988484dff3e32ec08249c76d22f78b92
DIDER <Simon.dider@gmail.com>
2024-11-21 09:49:20 +0100

    Tilføjelse af test af samlet pris for deltagelse

--- src/gui/App.java
+++ src/gui/App.java
@@ -1,14 +1,17 @@
 package gui;
 
 import application.controller.Controller;
+import application.model.Deltager;
+import application.model.Hotel;
 import application.model.Konference;
+import application.model.Tilmelding;
 
 import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) {
         initStorage();
-
+        testGetSamletPrisForDeltagelse();
     }
 
     public static void initStorage(){
@@ -16,5 +19,20 @@ public class App {
         Controller.createHotel("Den Hvide Svane","XX",1050,1250,0,50,0,havOgHimmel);
         Controller.createHotel("Høtel Phønix","XX",700,800,200,75,0,havOgHimmel);
         Controller.createHotel("Pension Tusindfryd","XX",500,600,0,0,100,havOgHimmel);
+    }
+
+    public static void testGetSamletPrisForDeltagelse() {
+        Konference konference = Controller.createKonference("Test Konference", "Test Adresse", LocalDate.of(2024, 12, 16), LocalDate.of(2024, 12, 18), 350);
+
+        Deltager deltager = Controller.createDeltager("Test Deltager", "Test Adresse", "12345678");
+
+        Tilmelding tilmelding = Controller.createTilmelding(LocalDate.of(2024, 12, 16), deltager, LocalDate.of(2024, 12, 18), false, konference);
+
+        Hotel hotel = Controller.createHotel("Test Hotel", "Test Adresse", 1000, 1500, 100, 50, 200, konference);
+        tilmelding.setHotel(hotel, true, true, true);
+
+        int samletPris = tilmelding.getSamletPrisForDeltagelse();
+
+        System.out.println("Samlet pris for deltagelse: " + samletPris);
     }
 }
