cd7965dca82696af0dc00c2dc7caf8f84ff86d29
DIDER <Simon.dider@gmail.com>
2024-11-21 11:54:37 +0100

    Tilføjelse af componnents og show vinduer i GUI samt ændring i App

--- src/gui/show/DeltagerPane.java
+++ src/gui/show/DeltagerPane.java
@@ -7,6 +7,10 @@ import javafx.geometry.Insets;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import Application.model.Deltager;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class DeltagerPane extends GridPane {
     public DeltagerPane() {
@@ -18,10 +22,20 @@ public class DeltagerPane extends GridPane {
         VBox detailsBox = new VBox();
         detailsBox.setSpacing(5);
         detailsBox.setPadding(new Insets(0, 5, 10, 10));
-        AttributeDisplay nameDisplay = new AttributeDisplay("Navn", "");
-        AttributeDisplay premierDate = new AttributeDisplay("Premier dato", "");
-        AttributeDisplay endDate = new AttributeDisplay("Slutdato", "");
-        detailsBox.getChildren().addAll(nameDisplay, premierDate, endDate);
+        AttributeDisplay navnDisplay = new AttributeDisplay("Navn", "");
+        AttributeDisplay tlfDisplay = new AttributeDisplay("Telefon nummer", "");
+        AttributeDisplay ledsagerDisplay = new AttributeDisplay("Ledsager", "");
+
+        detailsBox.getChildren().addAll(navnDisplay, tlfDisplay, ledsagerDisplay);
         this.add(detailsBox, 1, 0);
+        konferenceListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                navnDisplay.setValue(newValue.toString());
+                DateTimeFormatter longDateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
+                tlfDisplay.setValue(newValue.getPremierDate().format(longDateFormat));
+                ledsagerDisplay.setValue(newValue.get);
+            }
+        });
     }
+
 }
